12654091fc2598a5daf91596fd69a8058bd91aed
Tonny Frisk <tonny035@hotmail.com>
2019-11-19 13:03:19 +0100

    fix schoolAccessLocal

--- src/main/java/se/alten/schoolproject/dao/SchoolAccessLocal.java
+++ src/main/java/se/alten/schoolproject/dao/SchoolAccessLocal.java
@@ -1,5 +1,7 @@
 package se.alten.schoolproject.dao;
 
+import se.alten.schoolproject.exceptions.BadRequestException;
+import se.alten.schoolproject.exceptions.NotFoundException;
 import se.alten.schoolproject.model.StudentModel;
 
 import javax.ejb.Local;
@@ -8,7 +10,7 @@ import java.util.List;
 @Local
 public interface SchoolAccessLocal {
     List listAllStudents() throws Exception;
-    StudentModel addStudent(String studentModel);
-    void removeStudent(String student);
-    void updateStudent(String foreName, String lastName, String email);
+    StudentModel addStudent(String studentModel) throws BadRequestException;
+    void removeStudent(String student) throws NotFoundException;
+    void updateStudent(String foreName, String lastName, String email) throws NotFoundException;
 }
