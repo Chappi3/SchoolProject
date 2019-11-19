215c6f3afa2adbcd1efe4797399c9384a7a39b31
Tonny Frisk <tonny035@hotmail.com>
2019-11-19 13:05:06 +0100

    fix studentTransactionAccess

--- src/main/java/se/alten/schoolproject/transaction/StudentTransactionAccess.java
+++ src/main/java/se/alten/schoolproject/transaction/StudentTransactionAccess.java
@@ -1,6 +1,8 @@
 package se.alten.schoolproject.transaction;
 
 import se.alten.schoolproject.entity.StudentEntity;
+import se.alten.schoolproject.exceptions.BadRequestException;
+import se.alten.schoolproject.exceptions.NotFoundException;
 
 import javax.ejb.Local;
 import java.util.List;
@@ -8,8 +10,8 @@ import java.util.List;
 @Local
 public interface StudentTransactionAccess {
     List listAllStudents();
-    StudentEntity addStudent(StudentEntity studentToAdd);
-    void removeStudent(String student);
-    void updateStudent(String foreName, String lastName, String email);
+    void addStudent(StudentEntity studentToAdd) throws BadRequestException;
+    void removeStudent(String student) throws NotFoundException;
+    void updateStudent(String foreName, String lastName, String email) throws NotFoundException;
     void updateStudentPartial(Student studentToUpdate);
 }
