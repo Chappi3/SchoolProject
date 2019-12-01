ef3ef3e740c06a01367911c93979935de63d3081
Tonny Frisk <tonny035@hotmail.com>
2019-12-01 18:13:10 +0100

    update subjectController with addStudent, removeStudent, removeSubject

--- src/main/java/se/alten/schoolproject/rest/SubjectController.java
+++ src/main/java/se/alten/schoolproject/rest/SubjectController.java
@@ -4,6 +4,7 @@ import lombok.NoArgsConstructor;
 import se.alten.schoolproject.dao.SchoolAccessLocal;
 import se.alten.schoolproject.exceptions.BadRequestException;
 import se.alten.schoolproject.exceptions.DuplicateEntityException;
+import se.alten.schoolproject.exceptions.NotFoundException;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
@@ -36,6 +37,49 @@ public class SubjectController {
             return Response.status(Response.Status.CONFLICT).entity(e.getMessage()).build();
         }
         catch (BadRequestException e) {
+            return Response.status(Response.Status.BAD_REQUEST).entity(e.getMessage()).build();
+        }
+    }
+
+    @PATCH
+    @Produces({"application/JSON"})
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("/add/student")
+    public Response addStudent(String jsonData) {
+        try {
+            return Response.ok(schoolAccessLocal.addStudentToSubject(jsonData)).build();
+        } catch (DuplicateEntityException e) {
+            return Response.status(Response.Status.CONFLICT).entity(e.getMessage()).build();
+        } catch (NotFoundException e) {
+            return Response.status(Response.Status.NOT_FOUND).entity(e.getMessage()).build();
+        } catch (BadRequestException e) {
+            return Response.status(Response.Status.BAD_REQUEST).entity(e.getMessage()).build();
+        }
+    }
+
+    @DELETE
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("/remove/student")
+    public Response removeStudent(String jsonData) {
+        try {
+            schoolAccessLocal.removeStudentFromSubject(jsonData);
+            return Response.ok().build();
+        } catch (NotFoundException e) {
+            return Response.status(Response.Status.NOT_FOUND).entity(e.getMessage()).build();
+        } catch (BadRequestException e) {
+            return Response.status(Response.Status.BAD_REQUEST).entity(e.getMessage()).build();
+        }
+    }
+
+    @DELETE
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response removeSubject(String jsonData) {
+        try {
+            schoolAccessLocal.removeSubject(jsonData);
+            return Response.ok().build();
+        } catch (NotFoundException e) {
+            return Response.status(Response.Status.NOT_FOUND).entity(e.getMessage()).build();
+        } catch (BadRequestException e) {
             return Response.status(Response.Status.BAD_REQUEST).entity(e.getMessage()).build();
         }
     }
